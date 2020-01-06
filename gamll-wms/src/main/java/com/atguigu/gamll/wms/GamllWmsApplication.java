ff2a615410b37b89cb6a2cbb2ab38dd7f8d5dba5
fly <fly@itheima.com>
2020-01-06 23:06:54 +0800

    谷粒商城后台系统的搭建

---
+++ gamll-wms/src/main/java/com/atguigu/gamll/wms/GamllWmsApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gamll.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamllWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamllWmsApplication.class, args);
+    }
+
+}
